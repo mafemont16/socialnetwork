68d7b9c8102b1df91d104d69ceb2a5e73c4e1e01
Maria <mafemont16@gmail.com>
2021-04-27 14:54:29 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,17 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $response = $PDO->query("SELECT id FROM user WHERE nickname ='$username' AND password='$password' ");
+  $users = $response->fetchAll();
+  $nbUsersWithThisPasswordAndNickname = count($users);
+  if ($nbUsersWithThisPasswordAndNickname == 1) {
+    $connectedUser = $users[0];
+    return $connectedUser['id'];
+  } else {
+    return -1;
+  }
+}
